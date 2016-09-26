9f18e803385b400018a47028ca012dfa74dca203
foleyj <foleyj@cisco.com>
2016-09-26 13:25:34 -0400

    Add check-in/out fields

--- vtsite/vehicles/models.py
+++ vtsite/vehicles/models.py
@@ -9,11 +9,25 @@ class User(models.Model):
     user_password_hash = models.CharField(max_length=64)
 
 class VehicleReservation(models.Model):
+    YES_NO = (
+            ('Y', 'Yes'),
+            ('N', 'No'),
+    )
     vehicle = models.ForeignKey(Vehicle)
     user = models.ForeignKey(User)
     check_out_dt = models.DateField()
     check_in_dt = models.DateField()
     odometer = models.IntegerField()
     destination = models.CharField(max_length=250)
+    comments_check_out = models.CharField(max_length=250)
+    comments_check_in = models.CharField(max_length=250)
+    tires_inspected = models.CharField(max_length=1, choices=YES_NO)
+    damage_check_out = models.CharField(max_length=1, choices=YES_NO)
+    damage_check_in = models.CharField(max_length=1, choices=YES_NO)
+    fuel_full_check_out = models.CharField(max_length=1, choices=YES_NO)
+    fuel_full_check_in = models.CharField(max_length=1, choices=YES_NO)
+    vehicle_clean = models.CharField(max_length=1, choices=YES_NO)
+    post_accident = models.CharField(max_length=250)
+
 
 
