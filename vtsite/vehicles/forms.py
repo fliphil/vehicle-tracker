cf653ca22576f9827ef57b36514759a209da79d9
Philip Perricone <phperric@cisco.com>
2016-11-07 12:02:05 -0500

    Check-out vehicle form working.
    Also changed the db models for now to allow quick testing.

--- vtsite/vehicles/forms.py
+++ vtsite/vehicles/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class DepartureForm(forms.Form):
     vehicle = forms.CharField(label='Vehicle', max_length=20)
-    user = forms.CharField(label='User', max_length=50)
+    user = forms.CharField(label='Username', max_length=50)
     odometer = forms.IntegerField(min_value=0, max_value=500000)
--- vtsite/vehicles/views.py
+++ vtsite/vehicles/views.py
@@ -3,35 +3,53 @@ from django.http import HttpResponseRedirect
 from .forms import DepartureForm
 from .models import Vehicle
 from .models import User
-from .models import VehicleReservation
+from .models import TripReservation
 
 
 def index(request):
     return render(request, 'vehicles/home.html', {})
 
 
+def receive_checkout_info(form):
+    """
+    Handle the form received from the checkout_vehicle post
+    request. Do some validation and then record the data in a
+    new VehicleReservation table entry.
+    :param form: Contains the form data.
+    :return:
+    """
+    # check whether it's valid:
+    if form.is_valid():
+        # process the data in form.cleaned_data as required
+        vehicle_form_data = form.cleaned_data['vehicle']
+        user_form_data = form.cleaned_data['user']
+        odo_form_data = form.cleaned_data['odometer']
+
+        # Lookup the vehicle and user from database
+        vehicle_entry = Vehicle.objects.get(vehicle_desc=vehicle_form_data)
+        user_entry = User.objects.get(user_name=user_form_data)
+
+        # Create a new reservation
+        reservation = TripReservation(vehicle=vehicle_entry, user=user_entry,
+                                      odometer=odo_form_data)
+        reservation.save()
+
+
 def checkout_vehicle(request):
+    """
+    Run this function after a vehicles/checkout_vehicles url redirect.
+    :param request: HTTP request from client
+    :return: HttpResponse with reservation status page
+    """
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = DepartureForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            vehicle_form_data = form.cleaned_data['vehicle']
-            user_form_data = form.cleaned_data['user']
-            odo_form_data = form.cleaned_data['odometer']
 
-            # Lookup the vehicle and user from database
-            vehicle_entry = Vehicle.objects.get(vehicle_desc=vehicle_form_data)
-            user_entry = User.objects.get(user_full_name=user_form_data)
+        # handle the form data
+        receive_checkout_info(form)
 
-            # Create a new reservation
-            reservation = VehicleReservation(vehicle=vehicle_entry, user=user_entry,
-                                             odometer=odo_form_data)
-            reservation.save()
-
-            # redirect to a new URL:
-            return HttpResponseRedirect('/home/')
+        # redirect to a new URL:
+        return HttpResponseRedirect('/vehicles')
 
     # if a GET (or any other method) we'll create a blank form
     else:
