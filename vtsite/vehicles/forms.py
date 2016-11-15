783181401c1eff42038615bfb4dd236ecdeadc89
elliemdaw <elliemdaw@gmail.com>
2016-11-14 20:43:52 -0500

    Adds more necessary fields to check in and check out pages Still needs styling and cleaning up

--- vtsite/vehicles/forms.py
+++ vtsite/vehicles/forms.py
@@ -2,6 +2,21 @@ from django import forms
 
 
 class DepartureForm(forms.Form):
-    vehicle = forms.CharField(label='Vehicle', max_length=20)
-    user = forms.CharField(label='Username', max_length=50)
-    odometer = forms.IntegerField(min_value=0, max_value=500000)
+    vehicle = forms.ChoiceField(label='Vehicle', choices=[(x, x) for x in range(1, 11)])
+    user = forms.CharField(label='Username', max_length=50, required=True)
+    destination = forms.CharField(label='Destination', max_length=50, required=True)
+    odometer = forms.IntegerField(min_value=0, max_value=500000, required=True)
+    isFuelFull = forms.BooleanField(label='Fuel Tank Filled', required=True)
+    wereTiresInspected = forms.BooleanField(label='Tires Inspected', required=True)
+    completedDamageInspection = forms.BooleanField(label='Checked for damage to vehicle', required=True)
+
+class ArrivalForm(forms.Form):
+    # can we prepopulate this field with the vehicle they checked out...?
+    vehicle = forms.ChoiceField(label='Vehicle', choices=[(x, x) for x in range(1, 11)])
+    user = forms.CharField(label='Username', max_length=50, required=True)
+    destination = forms.CharField(label='Destination', max_length=50, required=True)
+    odometer = forms.IntegerField(min_value=0, max_value=500000, required=True)
+    isFuelFull = forms.BooleanField(label='Fuel Tank Filled', required=True)
+    trash = forms.BooleanField(label='Is the vehicle free of trash?', required=True)
+    damageOrMechanicalProblems = forms.BooleanField(label='Reporting damage or mechanical problems', required=True)
+    comments = forms.CharField(label='Comments', max_length=500, required=False)
--- vtsite/vehicles/views.py
+++ vtsite/vehicles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import DepartureForm
+from .forms import ArrivalForm
 from .models import Vehicle
 from .models import User
 from .models import TripReservation
@@ -23,7 +24,40 @@ def receive_checkout_info(form):
         # process the data in form.cleaned_data as required
         vehicle_form_data = form.cleaned_data['vehicle']
         user_form_data = form.cleaned_data['user']
+        dest_form_data = form.cleaned_data['destination']
         odo_form_data = form.cleaned_data['odometer']
+        fuel_form_data = form.cleaned_data['isFuelFull']
+        tire_form_data = form.cleaned_data['wereTiresInspected']
+        damage_form_data = form.cleaned_data['completedDamageInspection']
+
+        # Lookup the vehicle and user from database
+        vehicle_entry = Vehicle.objects.get(vehicle_desc=vehicle_form_data)
+        user_entry = User.objects.get(user_name=user_form_data)
+
+        # Create a new reservation
+        reservation = TripReservation(vehicle=vehicle_entry, user=user_entry,
+                                      odometer=odo_form_data)
+        reservation.save()
+
+def receive_checkin_info(form):
+    """
+    Handle the form received from the checkout_vehicle post
+    request. Do some validation and then record the data in a
+    new VehicleReservation table entry.
+    :param form: Contains the form data.
+    :return:
+    """
+    # check whether it's valid:
+    if form.is_valid():
+        # process the data in form.cleaned_data as required
+        vehicle_form_data = form.cleaned_data['vehicle']
+        user_form_data = form.cleaned_data['user']
+        dest_form_data = form.cleaned_data['destination']
+        odo_form_data = form.cleaned_data['odometer']
+        fuel_form_data = form.cleaned_data['isFuelFull']
+        trash_form_data = form.cleaned_data['trash']
+        damage_form_data = form.cleaned_data['damageOrMechanicalProblems']
+        comments_form_data = form.cleaned_data['comments']
 
         # Lookup the vehicle and user from database
         vehicle_entry = Vehicle.objects.get(vehicle_desc=vehicle_form_data)
@@ -59,4 +93,22 @@ def checkout_vehicle(request):
 
 
 def checkin_vehicle(request):
-    return render(request, 'vehicles/checkin_vehicle.html', {})
+    """
+    Run this function after a vehicles/checkin_vehicles url redirect.
+    :param request: HTTP request from client
+    :return: HttpResponse with reservation status page
+    """
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ArrivalForm(request.POST)
+
+        # handle the form data
+        receive_checkin_info(form)
+
+        # redirect to a new URL:
+        return HttpResponseRedirect('/vehicles')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ArrivalForm()
+    return render(request, 'vehicles/checkin_vehicle.html', {'form': form})
