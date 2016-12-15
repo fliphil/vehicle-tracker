c8626a6bf1307c1a589ce0af689097e29fa617f2
Philip Perricone <prperri@gmail.com>
2016-12-15 14:56:25 -0500

    Added readonly field to admin page

--- vtsite/vehicles/admin.py
+++ vtsite/vehicles/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Vehicle, TripReservation, UserStatus, VehicleStatus
 
+
+class TripReservationAdmin(admin.ModelAdmin):
+    readonly_fields = ('time_check_out',)
+
+
 admin.site.register(Vehicle)
-admin.site.register(TripReservation)
+admin.site.register(TripReservation, TripReservationAdmin)
 admin.site.register(UserStatus)
 admin.site.register(VehicleStatus)
