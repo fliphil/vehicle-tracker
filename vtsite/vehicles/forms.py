78dbf19068d53000fa8615b1834eb0d8c987e5a8
Philip Perricone <prperri@gmail.com>
2017-07-21 13:38:40 -0400

    Using reverse url, redo of the logo

--- vtsite/vehicles/forms.py
+++ vtsite/vehicles/forms.py
@@ -4,12 +4,24 @@ from .models import VehicleStatus
 
 
 class TripBeginForm(forms.Form):
-    vehicle = forms.CharField(label='License Plate', max_length=20, required=True)
-    destination = forms.CharField(label='Destination', max_length=200, required=True)
-    odometer = forms.IntegerField(min_value=0, max_value=500000, required=True)
-    is_fuel_full = forms.BooleanField(label='Fuel Tank Filled', required=True)
-    were_tires_inspected = forms.BooleanField(label='Tires Inspected', required=True)
-    completed_damage_inspection = forms.BooleanField(label='Checked for vehicle damage', required=True)
+    vehicle = forms.CharField(label='License Plate',
+                              max_length=20,
+                              required=True)
+    destination = forms.CharField(label='Destination',
+                                  max_length=200,
+                                  required=True)
+    odometer = forms.IntegerField(min_value=0,
+                                  max_value=500000,
+                                  required=True)
+    is_fuel_full = forms.BooleanField(label='Fuel tank full',
+                                      initial=False,
+                                      required=False)
+    were_tires_inspected = forms.BooleanField(label='Inspected tires',
+                                              initial=False,
+                                              required=False)
+    completed_damage_inspection = forms.BooleanField(label='Checked for damage',
+                                                     initial=False,
+                                                     required=False)
 
     def clean(self):
         """
@@ -41,12 +53,15 @@ class TripFinishForm(forms.Form):
     odometer = forms.IntegerField(min_value=0,
                                   max_value=500000,
                                   required=True)
-    is_fuel_full = forms.BooleanField(label='Fuel Tank Filled',
-                                      required=True)
-    trash = forms.BooleanField(label='Is the vehicle free of trash?',
-                               required=True)
+    is_fuel_full = forms.BooleanField(label='Fuel tank full',
+                                      initial=False,
+                                      required=False)
+    trash = forms.BooleanField(label='Free of trash',
+                               initial=False,
+                               required=False)
     damage_or_mechanical_problems = forms.BooleanField(label='Reporting damage or mechanical problems',
-                                                       required=True)
+                                                       initial=False,
+                                                       required=False)
     comments = forms.CharField(label='Comments',
                                widget=forms.Textarea(attrs={'rows':4, 'cols': 27}),
                                max_length=500,
